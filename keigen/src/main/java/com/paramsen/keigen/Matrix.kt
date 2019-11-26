17e1f92d0013ddc61e551ba64e0d26db63c0d55e
Pär Amsen <par.nils.amsen@gmail.com>
2019-11-27 00:18:36 +0100

    init C++ Matrix from float array w/ strides, Kotlin tests

--- keigen/src/main/java/com/paramsen/keigen/Matrix.kt
+++ keigen/src/main/java/com/paramsen/keigen/Matrix.kt
@@ -1,15 +1,17 @@
 package com.paramsen.keigen
 
+import androidx.annotation.IntRange
+
 /**
  * @author Pär Amsen 11/2019
  */
-class Matrix(var rows: Int, var cols: Int, var nativePointer: Long) {
-    constructor(rows: Int, cols: Int, fill: Float = 0f): this(rows, cols, KeigenNativeBridge.initializeFill(rows, cols, fill))
-    constructor(rows: Int, cols: Int, data: FloatArray, outerStride: Int = 1, innerStride: Int = cols) : this(rows, cols, NULL_PTR) {
-        //TODO validate that supplied data has enough elements etc
+class Matrix(@IntRange(from = 1) var rows: Int, @IntRange(from = 1) var cols: Int, var nativePointer: Long) {
+    constructor(@IntRange(from = 1) rows: Int, @IntRange(from = 1) cols: Int, fill: Float = 0f): this(rows, cols, KeigenNativeBridge.initializeFill(rows, cols, fill))
+    constructor(@IntRange(from = 1) rows: Int, @IntRange(from = 1) cols: Int, data: FloatArray, @IntRange(from = 1) outerStride: Int = 1, @IntRange(from = 1) innerStride: Int = cols) : this(rows, cols, NULL_PTR) {
+        throwIfMatrixSizeExceedDataSize(data.size, rows, cols)
+        throwIfStrideOutsideBounds(data.size, rows, outerStride, innerStride)
         nativePointer = KeigenNativeBridge.initializeWithData(rows, cols, data, outerStride, innerStride)
     }
-
     //matrix operations:
     // +,
     operator fun plus(m: Matrix): Matrix {
@@ -84,6 +86,8 @@ class Matrix(var rows: Int, var cols: Int, var nativePointer: Long) {
     private fun throwIfDimensNotEqual(m: Matrix) = check(rows == m.rows && cols == m.cols) { "dimensions must equal (this: [$rows, $cols], other: [${m.rows}, ${m.cols}])" }
     private fun throwIfOutsideBounds(row: Int, col: Int) = check(row < rows && col < cols) { "index overflow (access [$row, $col], bounds [${rows}, ${cols}])" }
     private fun throwIfInvalidMultiplicationDimens(m: Matrix) = check(cols == m.rows) { "cols must equal rows ($cols, ${m.rows})" }
+    private fun throwIfMatrixSizeExceedDataSize(dataSize: Int, rows: Int, cols: Int) = check(dataSize >= rows * cols) { "matrix must fit inside data" }
+    private fun throwIfStrideOutsideBounds(dataSize: Int, rows: Int, outerStride: Int, innerStride: Int) = check(dataSize >= rows * outerStride + innerStride) { "strides must not overflow data" }
 
     companion object {
         const val NULL_PTR = 0L
